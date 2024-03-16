611314f7e67b7cc0011cc17d0e596e00c4fe35e1
AugustoCBecker <augustobecker02@gmail.com>
2024-03-16 09:12:13 -0300

    Adiciona Droput + reduz o momentum para 0.99

--- project_functions.py
+++ project_functions.py
@@ -426,17 +426,17 @@ def CR_CED_model(input_shape, norm_params = None, n_reps = 5, skip = True):
             # Salva o próximo ponto de origem dos dados da conexão skip
             skip_vertix = x
         x = Conv2D(18, (9, length),padding='valid', use_bias = False, **kwargs)(x)
-        x = BatchNormalization(momentum = 0.997, epsilon = 1e-6)(x)
+        x = BatchNormalization(momentum = 0.99, epsilon = 1e-6)(x)
         x = ReLU(negative_slope=0.01)(x)
-        #x = Dropout(0.1)(x)
+        x = Dropout(0.1)(x)
         x = Conv2D(30, (5, 1),padding='same', use_bias = False,**kwargs)(x)
-        x = BatchNormalization(momentum = 0.997, epsilon = 1e-6)(x)
+        x = BatchNormalization(momentum = 0.99, epsilon = 1e-6)(x)
         x = ReLU(negative_slope=0.01)(x)
-        #x = Dropout(0.1)(x)
+        x = Dropout(0.1)(x)
         x = Conv2DTranspose(length, (9, 1),padding='valid', use_bias = False, **kwargs)(x)
-        x = BatchNormalization(momentum = 0.997, epsilon = 1e-6)(x)
+        x = BatchNormalization(momentum = 0.99, epsilon = 1e-6)(x)
         x = ReLU(negative_slope=0.01)(x)
-        #x = Dropout(0.1)(x)
+        x = Dropout(0.1)(x)
         if k < n_reps - 1:
             # Faz o reshape de (129,1,8) para (129,8,1), mantendo a estrutura da próxima rede R-CED
             x = Reshape(input_shape)(x)
